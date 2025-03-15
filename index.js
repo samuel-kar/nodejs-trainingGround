d26255844d8c3ffbf8fe362d1db94b65cffe096b
samuel-kar <samuel.karlhager@hotmail.com>
2025-03-15 23:35:42 +0100

    shows variations of expressing a function - note the single line

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-function getAge(birthYear, currentYear){
+/*function getAge(birthYear, currentYear){
     return (currentYear - birthYear);
 }
+    */
+//another way to write, with anonymous function
+// but impossible to call this one without a name
+/*
+function (birthYear, currentYear){
+    return (currentYear - birthYear);
+}
+*/
+/* 
+const getAge = function (birthYear, currentYear){
+    return (currentYear - birthYear);
+}
+*/
 
-const age = getAge(1972, 2022);
-console.log("The age constant is: " + age);
-module.exports.getAge = getAge;
-
-const number1 = 1;
-const number2 = 2;
-const result = number1 + number2;
-console.log("The result is " + result);
-
-const text = "Here is some text to briefly enjoy";
-console.log(text);
-console.log("Hello" + "world");
+//getting rid of function keyword
+/*
+const getAge = (birthYear, currentYear) => {
+    return (currentYear - birthYear);
+}
+*/
+//
+//and finally, curly braces optional if only one line 
+//const getAge = (birthYear, currentYear) => return currentYear - birthYear;
+//
+//also return keyword is implied if only one line
+const getAge = (birthYear, currentYear) => currentYear - birthYear;
