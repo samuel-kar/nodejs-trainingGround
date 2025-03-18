17499350634067574dd8d35ebfd95cfb9845ba9e
samuel-kar <samuel.karlhager@hotmail.com>
2025-03-18 10:07:39 +0100

    adds filtering through an array - immutable

--- test.mjs
+++ test.mjs
@@ -230,4 +230,31 @@ describe("loops does things over and over", () => {
         assert.equal(newNames.length, 4);
         assert.equal(newNames[3], "Stefan");
     });
+    it("filter an array - mutable", () => {
+        //arrange
+        const numbers = [10, 23, 1, 33, 8, 12];
+        assert.equal(numbers.length, 6);
+
+        //act
+        for (let i=0;i < numbers.length; i++) {
+            if (numbers[i]>10) {
+                numbers.splice(i, 1);
+            }
+        }
+
+        //assert
+        assert.equal(numbers.length, 3);
+        assert.equal(numbers[2], 8);
+    });
+
+    it("filter an array - immutable", () => {
+        //arrange
+        const numbers = [10, 23, 1, 33, 8, 12];
+
+        //act
+        const highNumbers = numbers.filter((x) => x > 10);
+
+        //assert
+        assert.equal(highNumbers.length, 3);
+    });
 });
