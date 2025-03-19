d04b20838eac1821f69290f97fe02272079c8b88
samuel-kar <samuel.karlhager@hotmail.com>
2025-03-19 09:39:03 +0100

    adds last thing about promises and error handling

--- promises.mjs
+++ promises.mjs
@@ -2,10 +2,13 @@ import fetch from "node-fetch";
 
 fetch("https://randomuser.me/api/?results=30")
     .then((response) => response.json())
-    .then((data) => data.results)
+    .then((data) => {
+        throw new Error("This is the error that I made");
+    }) 
     .then((results) => console.log(`We got ${results.length} rows`));
+    .catch((err) => console.log("Something went wrong", { err }));
 
-import { readFile } froms "fs";
+import { readFile } from "fs";
 readFile("thisFileDoesntExists", "utf8", (err, file) => {
     if (err) {
         console.log("We got an error", { err });
